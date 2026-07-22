28b7d071d07db9f3f69038d6f0f10731bb190f5a
Hugo Marques <43884516+hggmarks@users.noreply.github.com>
2026-07-22 00:24:02 +0000

    adds wait-for-db script

---
+++ infra/scripts/wait-for-db.cjs
@@ -0,0 +1,19 @@
+const { exec } = require("node:child_process");
+
+require("node:child_process");
+
+function checkDatabase() {
+    exec('docker exec db-dev pg_isready', handleReturn)
+
+    function handleReturn(error, stdout, stderr){
+        if(stdout.search("accepting connections") === -1){
+            process.stdout.write(".");
+            checkDatabase();
+            return;
+        }
+        console.log("\n\n🟢 O banco de dados está pronto e aceitando conexões!\n");
+    }
+}
+
+process.stdout.write("\n\n🔴 Aguardando banco de dados aceitar conexão");
+checkDatabase();
