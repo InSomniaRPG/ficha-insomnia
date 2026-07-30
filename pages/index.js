d3ac59cc4e4a6e63176efaa0b3f7857dbd28eefe
Hugo Marques <43884516+hggmarks@users.noreply.github.com>
2026-07-30 17:12:04 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+import path from "path";
+
 function index(request, response) {
-  return "Insomnia-RPG";
+  const html = fs.readFileSync(
+    path.join(process.cwd(), "api", "index.html"),
+    "utf8"
+  );
+
+  response.setHeader("Content-Type", "text/html");
+  response.status(200).send(html);
 }
 
 export default index;
